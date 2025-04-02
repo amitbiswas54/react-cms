2677e15ca06378be10a26a895de1167df7a0bb9f
amit biswas <amit.biswas54@gmail.com>
2025-04-02 18:59:42 +0530

    tailwind added2

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <h1 className="text-3xl font-bold underline text-red-500 bg-slate-900">
+      Hello PMIT
     </h1>
     </>
   )
