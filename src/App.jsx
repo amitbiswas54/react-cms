c0cbb521aa8a367d90f14767fd3adcd667e21e16
amit biswas <amit.biswas54@gmail.com>
2025-04-03 12:41:01 +0530

    tailwind added3

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500 bg-slate-900">
-      Hello PMIT
+      Hello PMIT Amit
     </h1>
     </>
   )
